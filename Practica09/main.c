e93ab9daf3f824963ead0d7857066d242989233d
Jorge Benjumea Burillo <me@intron014.com>
2023-11-21 12:50:49 +0100

    Practica 9 - A loading bar to load the program (and annoy others)

--- Practica09/main.c
+++ Practica09/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define A 80
 const char nombres[8][10+1]={"jose","roberto","fernando","mario",
@@ -23,7 +25,31 @@ void rand_nom_comp(char [20+1]);
 void rand_ALUMNO(struct ALUMNO  *);
 unsigned es_fecha_valida(DATE);
 
-int main() {
-    printf("Hello, World!\n");
+
+int rand_loader(){
+    return rand()%10;
+}
+
+int bar_loader(int perc){
+    int i;
+    printf("[");
+    for(i=0;i<perc;i++){
+        printf("=");
+    }
+    for(i=perc;i<100;i++){
+        printf(" ");
+    }
+    printf("] %d %%\r",perc);
     return 0;
+}
+
+int main() {
+    srand(time(NULL));
+    printf("(c) I014 Corp.\n Press enter to start\n");
+    getchar();
+    for(int i = 0; i<100 ; ){
+        bar_loader(i);
+        i+=rand_loader();
+        system("sleep 0.1");
+    }
 }
